6109f6463048ec648f693eebf2c7520b25407f61
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-04-13 17:10:41 +0300

    Update WorkingWithInheritance.cpp

--- WorkingWithInheritance.cpp
+++ WorkingWithInheritance.cpp
@@ -3,96 +3,123 @@
 
 using namespace std;
 
-class Person {
+//task # 1 ======================================================================
+
+class Shape {
+public:
+	virtual double area() = 0;
+};
+
+class Parallelogram : public Shape {
 protected:
-	string name;
-	int age;
-	char gender;
-	float weight;
+	double _a, _h;
 public:
-	Person(string name, int age, char gender, float weight) {
-		this->name = name;
-		this->age = age;
-		this->gender = gender;
-		this->weight = weight;
+	Parallelogram(double a, double h)
+	{
+		this->_a = a;
+		this->_h = h;
+	};
+
+	double area () override 
+	{
+		return this->_a * this->_h;
 	}
-	void setName(string newName) {
-		this->name = newName;
+};
+class Circle : public Shape {
+protected: 
+	double _r;
+public:
+	Circle(double r) 
+	{
+		this->_r = r;
 	}
-	void setAge(int newAge) {
-		this->age = newAge;
-	}
-	void setWeight(float newWeight) {
-		this->weight = newWeight;
-	}
-	void printInfo() {
-		cout << "name: " << this->name << ",";
-		cout << " age: " << this->age << ",";
-		cout << " weight " << this->weight << ",";
-	}
-	string getName() {
-		return this->name;
+
+	double area () override 
+	{
+		return 3.14 * std::pow(this->_r, 2);
 	}
 };
 
-class Student : public Person {
-private:
-	int enrollYear;
-	string role = "Student";
-public:
-	Student(int enrollYear, string name, int age, char gender, float weight) :Person(name, age, gender, weight) {
-		this->enrollYear = enrollYear;
-	}
-	int getEnrollYear() {
-		return this->enrollYear;
-	}
-	void printInfo() {
-		Person::printInfo();
-		cout << " Enrolled years: " << this->enrollYear;
-	}
-	string getRole() {
-		return this->role;
+class Reactangle : public Parallelogram {
+public: 
+	Reactangle(double a, double h) : Parallelogram(a, h) {};
+
+	double area() override 
+	{
+		return (this->_a * this->_h);
 	}
 };
 
-class Teacher : public Person {
-private:
-	int enrollYear;
-	string role = "Teacher";
+class Square : public Parallelogram {
 public:
-	Teacher(string name, int age, char gender, float weight) :Person(name, age, gender, weight) {
-		this->enrollYear = enrollYear;
-	}
-	void printInfo() {
-		Person::printInfo();
-		cout << " Enrolled years: " << this->enrollYear;
-	}
-	string getRole() {
-		return this->role;
+	Square(double a, double h) : Parallelogram(a, h) {};
+
+	double area() override
+	{
+		return std::pow(this->_a, 2);
 	}
 };
 
-string authorize(string role) {
-	if (role == "Teacher") {
-		return "Teacher access";
+class Rhombus : public Parallelogram {
+public:
+	Rhombus(double a, double h) : Parallelogram(a, h) {};
+
+	double area() override
+	{
+		return this->_a * this->_h;
 	}
-	if (role == "Student") {
-		return "Student access";
+};
+
+// ==================================================================================
+
+class Car { //class Car
+protected: 
+	string _company;
+	string _model;
+	
+public:
+	Car() { cout << "Car::Car() called" << endl; } //Base constructor
+	Car(string company, string model) 
+	{
+		this->_company = company;
+		this->_model = model;
+		cout << "Car::Car(int) called" << endl;
 	}
-}
+
+	void printInfo() 
+	{
+		cout << "info";
+	}
+};
+
+class PassangerCar : virtual public Car { //class PassangerCar inherits Car
+public:
+	PassangerCar(string company, string model) :Car(company, model) {
+		cout << "PassangerCar::PassangerCar(int) called" << endl;
+	}
+};
+
+class Bus : virtual public Car { //class Bus inherits Car
+public:
+	Bus(string company, string model) :Car(company, model) {
+		cout << "Bus::Bus(int) called" << endl;
+	}
+};
+
+class MiniVan : public PassangerCar, public Bus { //class MiniVan inherits PassangerCar and Bus
+public:
+	MiniVan(string company, string model) :Bus(company, model), PassangerCar(company, model) {
+		cout << "MiniVan::MiniVan(int) called" << endl;
+	}
+};
 
 
-int main()
-{
+int main() {
+	Parallelogram par(3, 10);
+	Circle circ(25);
+	Reactangle rect(2, 4);
+	Square sq(4, 2);
+	Rhombus rhomb(3, 5);	
 
-	//task #1 create example of studen inherited from person
-	Student alex(2, "Alex", 18, 'm', 64);
-	alex.printInfo();
-
-
-	// task #2 create authoriztion system based on class type
-	Teacher martin("Martin", 64, 'm', 72);
-	cout << '\n' << authorize(martin.getRole()) << endl;
-	cout << authorize(alex.getRole());
-
-}
+	MiniVan batmobile("Wayne inc", "tank");	
+};
